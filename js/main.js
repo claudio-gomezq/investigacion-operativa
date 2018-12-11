f662f8d78962ce536b2f9ec92573757c93a1a717
Claudio <claudio.gomezquidel@gmail.com>
2018-12-11 03:49:16 -0300

    maximizacion

--- js/main.js
+++ js/main.js
@@ -10,10 +10,63 @@ function main(){
   function calculate(){
     const data = getData();
     console.log(data);
-    const pivotColumn = findPivotColumn(data);
-    const pivotIndex = findPivotRow(pivotColumn, data);
+    let exit = false;
+    while(!exit){
+      const pivotColumn = findPivotColumn(data);
+      const pivotRow = findPivotRow(pivotColumn, data);
+      
+      console.log(data[pivotRow][pivotColumn]);
+      const pivotElement = data[pivotRow][pivotColumn];
+  
+      const newRow = reducePivotRowToOne(data[pivotRow], pivotElement);
+      data.splice(pivotRow, 1, newRow);
+  
+      for(let i = 0; i < data.length; i++){
+        if(i != pivotRow){
+          const reducedRow = reduceRowToOne(data, pivotRow, pivotColumn, i);
+          data.splice(i, 1, reducedRow);
+        }
+  
+      }
+      
+      if(checkFinish(data)){
+        exit = true;
+      }
+
+    }
+  }
+
+  function checkFinish(data){
+    const row = data[0];
+    let count = 0;
+    for(let item of row){
+       if(item == 0){
+        count++;
+       }
+    }
+    if(count == data[0].length-1){
+      return true;
+    }
+    return false;
+  }
+
+
+  function reduceRowToOne(data, pivotRow, pivotColumn, i){
+    const rowToReduce = data[i];
+    const cellToReduce = data[i][pivotColumn]*-1;
+    const pivotRowMod = data[pivotRow].map(item => item*cellToReduce);
     
-    console.log(data[pivotIndex][pivotColumn], pivotColumn, pivotIndex);
+    const reducedRow = [];
+    for(let i = 0; i<rowToReduce.length; i++){
+      const reducedCell = pivotRowMod[i] + rowToReduce[i];
+      reducedRow.push(reducedCell);
+    }
+    return reducedRow;
+  }
+
+  function reducePivotRowToOne(row, pivotElement){
+    const newRow = row.map(number => number / pivotElement);
+    return newRow;
 
   }
 
@@ -36,13 +89,13 @@ function main(){
   }
 
   function findPivotColumn(data){
-      const row = data[0];
+      const row = [...data[0]];
       row.pop();
       const minValue = Math.min(...row)
       return row.findIndex(item => item == minValue);
   }
   function findPivotRow(pivotColumn, data){
-    const rowLength = data[0].length;
+    const rowLength = data[0].length-1;
     const ratios= [];
     for(let i = 1; i<data.length; i++){
       const cell = data[i][pivotColumn];
